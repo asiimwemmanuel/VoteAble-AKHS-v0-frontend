7541cd0083b421d4f13e2c05c963aaa7fc103655
Joshua Mukisa <kiryowajoshua22@gmail.com>
2023-08-18 15:37:59 +0300

    Comitted

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,14 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 500, // Adjust this value as needed
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
   },
 });
