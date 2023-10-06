78648f1f8fbd3fb46f44008662a6c74bc899c30b
Joshua Mukisa <kiryowajoshua22@gmail.com>
2023-10-06 09:20:20 +0300

    Committed

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -64,7 +64,7 @@ export default function Login() {
     }
 
     localStorage.setItem('Student_ID', name);
-    localStorage.setItem('name', `${user.name}`);
+    localStorage.setItem('name', `${data2.name}`);
     localStorage.setItem('password', password);
     localStorage.setItem('gender', selectedGender);
 
