bca51ce60b5a4f063205f232a5c08bb05266758e
Joshua Mukisa <kiryowajoshua22@gmail.com>
2023-10-06 11:01:55 +0300

    Committed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -76,7 +76,8 @@ function Home() {
         future of voting to the hands of the students. Headed by: <br />
         <b>Joshua Mukisa - CEO, IB1</b> <br />
         <b>Akhil Muni - CFO, IB1</b> <br/>
-        <b>Rusheel Savani - COO, IB1</b>
+        <b>Rusheel Savani - COO, IB1</b> <br/>
+        {/* <b>Alishaan Bhud - COO, IB1</b> */}
       </p>
       <div className="hc">
         <div
